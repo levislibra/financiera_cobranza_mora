b1c07a89eac2a96c941d7d481edc62c8b3c76883
Levis <levislibra@hotmail.com>
2023-09-06 16:35:21 +0000

    UPdate

--- wizards/res_partner_cobranza_externa_wizard.py
+++ wizards/res_partner_cobranza_externa_wizard.py
@@ -18,7 +18,4 @@ class ResPartnerCobranzaExternaWizard(models.TransientModel):
 		active_ids = context.get('active_ids')
 		for _id in active_ids:
 			partner_id = self.env['res.partner'].browse(_id)
-			if partner_id.cobranza_externa_id:
-				partner_id.cobranza_externa_id = self.cobranza_externa_id.id
-			else:
-				partner_id.cobranza_externa_id = False
+			partner_id.cobranza_externa_id = self.cobranza_externa_id.id if self.cobranza_externa_id else False
