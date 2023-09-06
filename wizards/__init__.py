ef619bf9b66b56404513b25d436bfef3d54a9af6
Levis <levislibra@hotmail.com>
2023-09-06 03:04:24 +0000

    Update

--- wizards/__init__.py
+++ wizards/__init__.py
@@ -1,1 +1,2 @@
 from . import res_partner_debt_to_bank_file_wizard
+from . import res_partner_cobranza_externa_wizard
---
+++ wizards/res_partner_cobranza_externa_wizard.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields, api
+from datetime import datetime, timedelta
+from dateutil import relativedelta
+from openerp.exceptions import UserError, ValidationError
+import time
+import math
+
+class ResPartnerCobranzaExternaWizard(models.TransientModel):
+	_name = 'res.partner.cobranza.externa.wizard'
+
+	cobranza_externa_id = fields.Many2one('financiera.cobranza.externa', 'Cobranza externa')
+	
+	@api.multi
+	def asignar_cobranza_externa(self):
+		context = dict(self._context or {})
+		active_ids = context.get('active_ids')
+		for _id in active_ids:
+			partner_id = self.env['res.partner'].browse(_id)
+			partner_id.cobranza_externa_id = self.cobranza_externa_id.id
